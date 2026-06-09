f54f08afee389a49dc9f694d9253ac80cf127e66
insopti <xinaise.7ix@gmail.com>
2026-06-09 22:31:44 +0200

    Update Set-PSExecutionScript.bat

--- Set-PSExecutionScript.bat
+++ Set-PSExecutionScript.bat
@@ -1,35 +1,43 @@
 @echo off & setlocal
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 if '%errorlevel%' NEQ '0' (goto uacprompt) else (goto gotadmin)
+
 :uacprompt
 echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
 echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
 "%temp%\getadmin.vbs" & del "%temp%\getadmin.vbs" & exit /B
+
 :gotadmin
 if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs"
-pushd "%CD%" & CD /D "%~dp0" & cls
+pushd "%CD%" & CD /D "%~dp0"
 
 title Enable/Disable PowerShell Script Execution Authorization. ^| [https://github.com/insovs/EnablePowerShellScript]
+
+:menu
+cls
+set "c="
 echo.
-echo   *PowerShell Script Execution Authorization.
+echo     * PowerShell Script Execution Authorization
+echo      [https://github.com/insovs/EnablePowerShellScript]
 echo.
-echo   By default, Windows blocks the execution of PowerShell scripts for security reasons.
-echo   This program will modify the execution policy (ExecutionPolicy) of this machine
-echo   so that you can run PowerShell scripts. [https://github.com/insovs/EnablePowerShellScript]
+echo     By default, Windows blocks the execution of PowerShell scripts for security reasons.
+echo     This program will modify the execution policy (ExecutionPolicy) of this machine 
+echo     so that you can run PowerShell scripts without interruption.
 echo.
-echo  [1] Enable  (Unrestricted + UAC off + SAC off)
-echo  [2] Revert  (Restricted Only)
-echo  [3] Exit
+echo    [1] Enable - (Unrestricted + UAC off + SAC off)
+echo    [2] Revert - (Restricted Only)
+echo    [3] Exit
 echo.
 set /p c=">> "
 if "%c%"=="1" goto enable
 if "%c%"=="2" goto revert
 if "%c%"=="3" goto end
-echo  Invalid choice. & timeout /t 2 >nul & goto gotadmin
+echo Invalid choice. & timeout /t 2 >nul & goto menu
 
 :enable
 cls
-reg add "HKCR\Applications\powershell.exe\shell\open\command" /ve /t REG_SZ /d "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoLogo -ExecutionPolicy unrestricted -File \"%%1\"" /f >nul
+echo  [*] Unblocking PowerSwhell Execution Script...
+reg add "HKCR\Applications\powershell.exe\shell\open\command" /ve /t REG_SZ /d "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoLogo -ExecutionPolicy unrestricted -File \"\"\"%%1\"\"\"" /f >nul
 reg add "HKCU\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell" /v ExecutionPolicy /t REG_SZ /d Unrestricted /f >nul
 reg add "HKLM\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell" /v ExecutionPolicy /t REG_SZ /d Unrestricted /f >nul
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\PowerShell" /v ExecutionPolicy /t REG_SZ /d Unrestricted /f >nul
@@ -41,11 +49,14 @@ reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\PowerShell\Transcription" /v E
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v EnableLUA /t REG_DWORD /d 0 /f >nul
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\CI\Policy" /v VerifiedAndReputablePolicyState /t REG_DWORD /d 0 /f >nul
 powershell -Command "Get-ChildItem -Path . -Recurse | Unblock-File" >nul 2>&1
-echo  [+] Done. Restart to apply changes.
+
+echo  [+] Done. Restart necessary to apply full changes.
 goto restart
 
 :revert
 cls
+echo  [*] Reverting Execution Policy...
+reg delete "HKCR\Applications\powershell.exe\shell\open\command" /ve /f >nul 2>&1
 reg add "HKCU\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell" /v ExecutionPolicy /t REG_SZ /d Restricted /f >nul
 reg add "HKLM\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell" /v ExecutionPolicy /t REG_SZ /d Restricted /f >nul
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\PowerShell" /v ExecutionPolicy /t REG_SZ /d Restricted /f >nul
@@ -54,13 +65,16 @@ reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\PowerShell\ModuleLogging" /v E
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging" /v EnableScriptBlockLogging /t REG_DWORD /d 1 /f >nul
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging" /v EnableScriptBlockInvocationLogging /t REG_DWORD /d 1 /f >nul
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\PowerShell\Transcription" /v EnableTranscripting /t REG_DWORD /d 1 /f >nul
-echo  [+] Reverted. Restart to apply changes.
+echo  [+] Reverted. Restart necessary to apply full changes.
 goto restart
 
 :restart
 echo.
-set /p r="  Restart now? (Y/N) >> "
+set "r="
+set /p r="  Restart now ? (Y/N) >> "
+echo Restart necessary to apply full changes.
 if /i "%r%"=="y" shutdown /r /t 0
+goto end
 
 :end
 endlocal & exit /B
